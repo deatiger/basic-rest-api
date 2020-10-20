143a28632d13f85a633bf4a1ec0b4f2efb792328
deatiger <deatiger1492@gmail.com>
2020-10-20 10:22:46 +0900

    Add post/put/delete methods to write data to sqlite3

--- app/app.js
+++ app/app.js
@@ -2,9 +2,15 @@ const express = require('express')
 const app = express()
 const sqlite3 = require('sqlite3')
 const path = require('path')
+const bodyParser = require('body-parser')
 
 const dbPath = "app/db/database.sqlite3"
 
+// リクエストのbodyをパースする設定
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+
+// publicディレクトリを静的ファイル群のルートディレクトリとして設定
 app.use(express.static(path.join(__dirname, 'public')))
 
 // Get all users
@@ -41,6 +47,78 @@ app.get('/api/v1/search', (req, res) => {
   db.all(`SELECT * FROM users WHERE name LIKE "%${keyword}%"`, (err, rows) => {
     res.json(rows)
   })
+
+  db.close()
+})
+
+const run = async (sql, db, res, message) => {
+  return new Promise((resolve, reject) => {
+    db.run(sql, (err) => {
+      if (err) {
+        res.status(500).send(err)
+        return reject()
+      } else {
+        res.json({message: message})
+        return resolve()
+      }
+    })
+  })
+}
+
+// Create a new user
+app.post('/api/v1/users', async (req, res) => {
+  // Connect database
+  const db = new sqlite3.Database(dbPath)
+
+  const name = req.body.name
+  const profile = req.body.profile ? req.body.profile : ""
+  const dateOfBirth = req.body.date_of_birth ? req.body.date_of_birth : ""
+
+  await run(
+    `INSERT INTO users (name, profile, date_of_birth) VALUES ("${name}", "${profile}", "${dateOfBirth}")`,
+    db,
+    res,
+    "新規ユーザーを作成しました！"
+  )
+
+  db.close()
+})
+
+// Update user data
+app.put('/api/v1/users/:id', async (req, res) => {
+  // Connect database
+  const db = new sqlite3.Database(dbPath)
+  const id = req.params.id
+
+  // 現在のユーザー情報を取得する
+  db.get(`SELECT * FROM users WHERE id=${id}`, async (err, row) => {
+    const name = req.body.name ? req.body.name : row.name
+    const profile = req.body.profile ? req.body.profile : row.profile
+    const dateOfBirth = req.body.date_of_birth ? req.body.date_of_birth : row.date_of_birth
+
+    await run(
+      `UPDATE users SET name="${name}", profile="${profile}", date_of_birth="${dateOfBirth}" WHERE id=${id}`,
+      db,
+      res,
+      "ユーザー情報を更新しました！"
+    )
+  })
+
+  db.close()
+})
+
+// Delete user data
+app.delete('/api/v1/users/:id', async (req, res) => {
+  // Connect database
+  const db = new sqlite3.Database(dbPath)
+  const id = req.params.id
+
+  await run(
+    `DELETE FROM users WHERE id=${id}`,
+    db,
+    res,
+    "ユーザー情報を削除しました！"
+  )
 
   db.close()
 })
