8efa785a8bd1e99808b890ffe0c035ade9986cd4
deatiger <deatiger1492@gmail.com>
2020-10-24 10:44:20 +0900

    Add features to edit and delete user

--- app/public/js/index.js
+++ app/public/js/index.js
@@ -20,6 +20,21 @@ const indexModule = (() => {
       })
       break;
 
+    case '/edit.html':
+      const uid = window.location.search.split('?uid=')[1]
+
+      document.getElementById('save-btn').addEventListener('click', () => {
+        return usersModule.saveUser(uid)
+      })
+      document.getElementById('cancel-btn').addEventListener('click', () => {
+        return window.location.href = '/'
+      })
+      document.getElementById('delete-btn').addEventListener('click', () => {
+        return usersModule.deleteUser(uid)
+      })
+
+      return usersModule.setExistingValue(uid)
+
     default:
       break;
   }
--- app/public/js/users.js
+++ app/public/js/users.js
@@ -20,6 +20,7 @@ const usersModule = (() => {
                         <td>${user.date_of_birth}</td>
                         <td>${user.created_at}</td>
                         <td>${user.updated_at}</td>
+                        <td><a href="edit.html?uid=${user.id}">編集</a></td>
                       </tr>`
         document.getElementById('users-list').insertAdjacentHTML('beforeend', body)
       }
@@ -46,6 +47,54 @@ const usersModule = (() => {
 
       alert(resJson.message)
       window.location.href = "/"
+    },
+    setExistingValue: async (uid) => {
+      const res = await fetch(BASE_URL + "/" + uid)
+      const resJson = await res.json()
+
+      document.getElementById('name').value = resJson.name
+      document.getElementById('profile').value = resJson.profile
+      document.getElementById('date-of-birth').value = resJson.date_of_birth
+    },
+    saveUser: async (uid) => {
+      const name = document.getElementById("name").value
+      const profile = document.getElementById("profile").value
+      const dateOfBirth = document.getElementById("date-of-birth").value
+
+      // リクエストのbody
+      const body = {
+        name: name,
+        profile: profile,
+        date_of_birth: dateOfBirth
+      }
+
+      const res = await fetch(BASE_URL + "/" + uid, {
+        method: "PUT",
+        headers: headers,
+        body: JSON.stringify(body)
+      })
+
+      const resJson = await res.json()
+
+      alert(resJson.message)
+      window.location.href = "/"
+    },
+    deleteUser: async (uid) => {
+      const ret = window.confirm('このユーザーを削除しますか？')
+
+      if (!ret) {
+        return false
+      } else {
+        const res = await fetch(BASE_URL + "/" + uid, {
+          method: "DELETE",
+          headers: headers
+        })
+
+        const resJson = await res.json()
+
+        alert(resJson.message)
+        window.location.href = '/'
+      }
     }
   }
 })()
