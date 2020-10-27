031984dd1ab6983e96f404dbc01117b4f3633cf1
deatiger <deatiger1492@gmail.com>
2020-10-27 09:32:53 +0900

    Add error handlings

--- app/app.js
+++ app/app.js
@@ -32,7 +32,11 @@ app.get('/api/v1/users/:id', (req, res) => {
   const id = req.params.id
 
   db.get(`SELECT * FROM users WHERE id = ${id}`, (err, row) => {
-    res.json(row)
+    if (!row) {
+      res.status(404).send({error: "Not Found!"})
+    } else {
+      res.status(200).json(row)
+    }
   })
 
   db.close()
@@ -51,14 +55,12 @@ app.get('/api/v1/search', (req, res) => {
   db.close()
 })
 
-const run = async (sql, db, res, message) => {
+const run = async (sql, db) => {
   return new Promise((resolve, reject) => {
     db.run(sql, (err) => {
       if (err) {
-        res.status(500).send(err)
-        return reject()
+        return reject(err)
       } else {
-        res.json({message: message})
         return resolve()
       }
     })
@@ -67,44 +69,63 @@ const run = async (sql, db, res, message) => {
 
 // Create a new user
 app.post('/api/v1/users', async (req, res) => {
-  // Connect database
-  const db = new sqlite3.Database(dbPath)
+  if (!req.body.name || req.body.name === "") {
+    res.status(400).send({error: "ユーザー名が指定されていません。"})
+  } else {
+    // Connect database
+    const db = new sqlite3.Database(dbPath)
 
-  const name = req.body.name
-  const profile = req.body.profile ? req.body.profile : ""
-  const dateOfBirth = req.body.date_of_birth ? req.body.date_of_birth : ""
+    const name = req.body.name
+    const profile = req.body.profile ? req.body.profile : ""
+    const dateOfBirth = req.body.date_of_birth ? req.body.date_of_birth : ""
 
-  await run(
-    `INSERT INTO users (name, profile, date_of_birth) VALUES ("${name}", "${profile}", "${dateOfBirth}")`,
-    db,
-    res,
-    "新規ユーザーを作成しました！"
-  )
+    try {
+      await run(
+        `INSERT INTO users (name, profile, date_of_birth) VALUES ("${name}", "${profile}", "${dateOfBirth}")`,
+        db
+      )
+      res.status(201).send({message: "新規ユーザーを作成しました。"})
+    } catch (e) {
+      res.status(500).send({error: e})
+    }
 
-  db.close()
+    db.close()
+  }
 })
 
 // Update user data
 app.put('/api/v1/users/:id', async (req, res) => {
-  // Connect database
-  const db = new sqlite3.Database(dbPath)
-  const id = req.params.id
+  if (!req.body.name || req.body.name === "") {
+    res.status(400).send({error: "ユーザー名が指定されていません。"})
+  } else {
+    // Connect database
+    const db = new sqlite3.Database(dbPath)
+    const id = req.params.id
 
-  // 現在のユーザー情報を取得する
-  db.get(`SELECT * FROM users WHERE id=${id}`, async (err, row) => {
-    const name = req.body.name ? req.body.name : row.name
-    const profile = req.body.profile ? req.body.profile : row.profile
-    const dateOfBirth = req.body.date_of_birth ? req.body.date_of_birth : row.date_of_birth
+    // 現在のユーザー情報を取得する
+    db.get(`SELECT * FROM users WHERE id=${id}`, async (err, row) => {
 
-    await run(
-      `UPDATE users SET name="${name}", profile="${profile}", date_of_birth="${dateOfBirth}" WHERE id=${id}`,
-      db,
-      res,
-      "ユーザー情報を更新しました！"
-    )
-  })
+      if (!row) {
+        res.status(404).send({error: "指定されたユーザーが見つかりません。"})
+      } else {
+        const name = req.body.name ? req.body.name : row.name
+        const profile = req.body.profile ? req.body.profile : row.profile
+        const dateOfBirth = req.body.date_of_birth ? req.body.date_of_birth : row.date_of_birth
 
-  db.close()
+        try {
+          await run(
+            `UPDATE users SET name="${name}", profile="${profile}", date_of_birth="${dateOfBirth}" WHERE id=${id}`,
+            db
+          )
+          res.status(200).send({message: "ユーザー情報を更新しました。"})
+        } catch (e) {
+          res.status(500).send({error: e})
+        }
+      }
+    })
+
+    db.close()
+  }
 })
 
 // Delete user data
@@ -113,12 +134,20 @@ app.delete('/api/v1/users/:id', async (req, res) => {
   const db = new sqlite3.Database(dbPath)
   const id = req.params.id
 
-  await run(
-    `DELETE FROM users WHERE id=${id}`,
-    db,
-    res,
-    "ユーザー情報を削除しました！"
-  )
+  // 現在のユーザー情報を取得する
+  db.get(`SELECT * FROM users WHERE id=${id}`, async (err, row) => {
+
+    if (!row) {
+      res.status(404).send({error: "指定されたユーザーが見つかりません。"})
+    } else {
+      try {
+        await run(`DELETE FROM users WHERE id=${id}`, db)
+        res.status(200).send({message: "ユーザーを削除しました。"})
+      } catch (e) {
+        res.status(500).send({error: e})
+      }
+    }
+  })
 
   db.close()
 })
--- app/public/js/users.js
+++ app/public/js/users.js
@@ -6,6 +6,37 @@ const usersModule = (() => {
   const headers = new Headers()
   headers.set("Content-Type", "application/json")
 
+  const handleError = async (res) => {
+    const resJson = await res.json()
+
+    switch (res.status) {
+      case 200:
+        alert(resJson.message)
+        window.location.href = "/"
+        break;
+      case 201:
+        alert(resJson.message)
+        window.location.href = "/"
+        break;
+      case 400:
+        // リクエストのパラメータ間違い
+        alert(resJson.error)
+        break;
+      case 404:
+        // 指定したリソースが見つからない
+        alert(resJson.error)
+        break;
+      case 500:
+        // サーバーの内部エラー
+        alert(resJson.error)
+        break;
+      default:
+        alert("何らかのエラーが発生しました。")
+        break;
+    }
+
+  }
+
   return {
     fetchAllUsers: async () => {
       const res = await fetch(BASE_URL)
@@ -43,10 +74,7 @@ const usersModule = (() => {
         body: JSON.stringify(body)
       })
 
-      const resJson = await res.json()
-
-      alert(resJson.message)
-      window.location.href = "/"
+      return handleError(res)
     },
     setExistingValue: async (uid) => {
       const res = await fetch(BASE_URL + "/" + uid)
@@ -74,10 +102,7 @@ const usersModule = (() => {
         body: JSON.stringify(body)
       })
 
-      const resJson = await res.json()
-
-      alert(resJson.message)
-      window.location.href = "/"
+      return handleError(res)
     },
     deleteUser: async (uid) => {
       const ret = window.confirm('このユーザーを削除しますか？')
@@ -90,10 +115,7 @@ const usersModule = (() => {
           headers: headers
         })
 
-        const resJson = await res.json()
-
-        alert(resJson.message)
-        window.location.href = '/'
+        return handleError(res)
       }
     }
   }
